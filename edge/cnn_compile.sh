a9f2413bff1b3823b21346cb17b1b6d4e3690b75
wiIliu <willowconnelly@oakland.edu>
2025-11-25 00:37:19 -0500

    edge - loading files

--- edge/cnn_compile.sh
+++ edge/cnn_compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vai_c_xir -x quantize_result/MultiClassAttackCNN_int.xmodel -a arch.json -o c_out_dpu -n cap_cnnMulti_first
+vai_c_xir -x quantize_result/MultiAttnCNN_int.xmodel -a arch.json -o c_out_dpu -n cap_multiAttnCNN_first
--- edge/testLoader.py
+++ edge/testLoader.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 import pandas as pd
-import torch.nn.functional as F
 from sklearn.calibration import LabelEncoder
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -23,24 +22,11 @@ X_test = test.drop("Attack_type", axis=1).values
 
 X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
 y_test_tensor = torch.tensor(y_test, dtype=torch.long) 
+X_test_tensor = X_test_tensor.unsqueeze(1)
+test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
 
-def pad_to_square(X, size=90):
-    pad_len = size - X.shape[1]
-    if pad_len > 0:
-        X = F.pad(X, (0, pad_len))
-        return X
-
-
-X_test_padded = pad_to_square(X_test_tensor)
-
-X_test_cnn = X_test_padded.view(-1, 1, 9, 10)
-
-
-test_dataset = TensorDataset(X_test_cnn, y_test_tensor)
-
-
-class_names = label_encoder.classes_
 test_loader = DataLoader(test_dataset, batch_size=64)
+class_names = label_encoder.classes_
 
 for x, y in test_loader:
     print(type(x))
