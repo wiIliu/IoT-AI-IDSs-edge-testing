7b35708bd8b579f698dac12dcfeaa57289ddad03
wiIliu <willowconnelly@oakland.edu>
2025-11-27 12:17:04 -0500

    edge - updated quantized input data

--- edge/edgeDataPreparer.py
+++ edge/edgeDataPreparer.py
@@ -1,6 +1,11 @@
 import torch
 import numpy as np
-from binaryTestLoader import test_loader
+### Binary
+# from binaryTestLoader import test_loader
+# classif_type="bianry"
+### Multi
+from multiTestLoader import test_loader
+classif_type="multi"
 
 # get ONE BATCH from test loader - can change to more
 x_batch, y_batch = next(iter(test_loader))
@@ -12,7 +17,7 @@ y_np = y_batch.numpy().astype(np.int64)
 
 print(x_np.shape)
 
-np.save("test_inputs.npy", x_np)
-np.save("test_labels.npy", y_np)
+np.save(f"{classif_type}_test_inputs.npy", x_np)
+np.save(f"{classif_type}_test_labels.npy", y_np)
 
-print("Saved test_inputs.npy and test_labels.npy")
+print(f"Saved {classif_type}_test_inputs.npy and {classif_type}_test_labels.npy")
